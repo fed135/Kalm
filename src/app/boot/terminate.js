7f47ead97f83137c44cf1d3acee1616051076320
Frederic <fredericcharette@gmail.com>
2015-09-07 00:04:33 -0400

    added extra kill after timeout

--- src/app/boot/terminate.js
+++ src/app/boot/terminate.js
@@ -4,16 +4,21 @@ function onTerminate(callback) {
 	var utils = K.getComponent('utils');
 
 	var servers = [];
-  cl.print('\r ');
+  cl.print('\r  ');
 	cl.warn('Shutting down...');
 
 	connection.listeners.forEach(function(e) {
 		servers.push(e.stop);
 	});
 
-	utils.async.all(servers, function() {
-		process.exit();
-	});
+	utils.async.all(servers, _kill);
+
+	//Just in case something goes bad
+	setTimeout(_kill, 2000);
+}
+
+function _kill() {
+	process.exit();
 }
 
 module.exports = onTerminate;
