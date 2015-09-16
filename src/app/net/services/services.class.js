510b1257af3cd75ad0a0daf9dde9a39405be6221
Frederic <fredericcharette@gmail.com>
2015-09-15 20:17:29 -0400

    started services init

--- src/app/net/services/services.class.js
+++ src/app/net/services/services.class.js
@@ -1,5 +1,11 @@
-function register() {
-	
+function register(serviceConf) {
+	console.log(this);
+	var request = K.getComponent('request');
+	var _self = this;
+
+	request.send(serviceConf, function(err, data) {
+		_self.list.push(data);
+	});
 }
 
 function unregister() {
@@ -14,6 +20,14 @@ function match(req) {
 
 }
 
+function load(callback) {
+	if (K.appConf.services && K.appConf.services.length) {
+		K.appConf.services.forEach(this.register);
+	}
+	//TODO: only callback when done
+	callback();
+}
+
 module.exports = {
 	pkgName: 'services',
 	attributes: {
@@ -23,6 +37,7 @@ module.exports = {
 		getKey: getKey,
 		register: register,
 		unregister: unregister,
-		match: match
+		match: match,
+		init: load
 	}
 };
