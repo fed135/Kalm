cad682fdd16b8292df731dd143be6fed25617f2c
Frederic <fredericcharette@gmail.com>
2016-03-29 19:04:00 -0400

    exposed loaders

--- src/index.js
+++ src/index.js
@@ -9,10 +9,16 @@
 
 var Client = require('./Client');
 var Server = require('./Server');
+var adapters = require('./adapters');
+var encoders = require('./encoders');
+var middleware = require('./middleware');
 
 /* Exports -------------------------------------------------------------------*/
 
 module.exports = {
 	Client: Client,
-	Server: Server
+	Server: Server,
+	adapters: adapters,
+	encoders: encoders,
+	middleware: middleware
 };
