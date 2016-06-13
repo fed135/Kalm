0aacb67b42eb1f3f2c368e6eb9918efe244c987b
Frederic <fredericcharette@gmail.com>
2016-06-12 22:36:44 -0400

    extracted store logic from adapter/encoder indexes

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -6,17 +6,17 @@
 
 /* Requires ------------------------------------------------------------------*/
 
-const debug = require('debug')('kalm');
+const Store = require('../Store');
 
 /* Methods -------------------------------------------------------------------*/
 
-class Adapters {
+class Adapters extends Store {
 
 	/**
 	 * Adapters constructor
 	 */
 	constructor() {
-		this.list = {};
+		super('adapter');
 
 		// If running in the browser, do not load net adapters
 		if (process.env.NODE_ENV !== 'browser') {
@@ -24,31 +24,6 @@ class Adapters {
 			this.list.tcp = require('./tcp');
 			this.list.udp = require('./udp');
 		}
-	}
-
-	/**
-	 * Returns the selected adapter
-	 * @param {string} adapter The name of the adapter to return
-	 * @returns {object|undefined} The adapter
-	 */
-	resolve(adapter) {
-		if (this.list.hasOwnProperty(adapter)) {
-			return this.list[adapter];
-		}
-		else {
-			debug('error: no adapter "' + adapter + '" found');
-			return;
-		}
-	}
-
-	/**
-	 * Registers a new adapter
-	 * @param {string} name The name of the adapter
-	 * @param {object} mod The body of the adapter
-	 */
-	register(name, mod) {
-		debug('log: registering new adapter "' + name + '":');
-		this.list[name] = mod;
 	}
 }
 
--- src/encoders/index.js
+++ src/encoders/index.js
@@ -8,43 +8,18 @@
 
 var json = require('./json');
 
-const debug = require('debug')('kalm');
+const Store = require('../Store');
 
 /* Methods -------------------------------------------------------------------*/
 
-class Encoders {
+class Encoders extends Store {
 
 	/**
 	 * Encoders constructor
 	 */
 	constructor() {
-		this.list = {
-			json: json
-		};
-	}
-
-	/**
-	 * Returns the selected encoder
-	 * @param {string} encoder The name of the encoder to return
-	 * @returns {object|undefined} The encoder
-	 */
-	resolve(encoder) {
-		if (this.list.hasOwnProperty(encoder)) {
-			return this.list[encoder];
-		}
-		else {
-			debug('error: no encoder "' + encoder + '" found');
-			return;
-		}
-	}
-
-	/**
-	 * Registers a new encoder
-	 * @param {string} name The name of the encoder
-	 * @param {object} mod The body of the encoder
-	 */
-	register(name, mod) {
-		this.list[name] = mod;
+		super('encoder');
+		this.list.json = json;
 	}
 }
 
