afe1ef4825360bbcf5415195b025603b14b3b97c
Frederic <fredericcharette@gmail.com>
2016-06-19 14:42:08 -0400

    reverted benchmark test duration

--- tests/benchmarks/settings.js
+++ tests/benchmarks/settings.js
@@ -4,7 +4,7 @@ module.exports = {
 	port: 3000,
 	bundlerDelay: 16,
 	bundlerMaxPackets: 2048,
-	testDuration: 1000 * 1,
+	testDuration: 1000 * 60,
 	testPayload: { foo: 'bar'},
 	testChannel: 'test'
 };
