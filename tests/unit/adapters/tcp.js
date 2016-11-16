5d1fe12180fc0d31bb1b446681c4f23d8739974a
Damon Perron-Laurin <damonperronlaurin@gmail.com>
2016-11-15 21:53:38 -0500

    fix tests with missing 'done' argument

--- tests/unit/adapters/tcp.js
+++ tests/unit/adapters/tcp.js
@@ -18,147 +18,134 @@ const EventEmitter = require('events').EventEmitter;
 /* Tests ---------------------------------------------------------------------*/
 
 describe('TCP', () => {
-    describe('#listen(server, callback)', () => {
+	describe('#listen(server, callback)', () => {
 
-        afterEach(() => sinon.mock.restore());
+		afterEach(() => sinon.mock.restore());
 
-        it('should bind to a defined port', (done) => {
+		it('should bind to a defined port', (done) => {
 
-            var netMock = sinon.mock(net);
-            var port = 9000;
-            var serverTest = {
-                listener: null,
-                handleError: function () {
-                },
-                handleRequest: function () {
-                },
-                options: {
-                    port: port
-                }
-            };
-            var listenerTest = sinon.mock({
-                on: function () {
-                },
-                listen: function (port, callback) {
-                    callback();
-                }
-            });
+			var netMock = sinon.mock(net);
+			var port = 9000;
+			var serverTest = {
+				listener: null,
+				handleError: function() {},
+				handleRequest: function() {},
+				options: {
+					port: port
+				}
+			};
+			var listenerTest = sinon.mock({
+				on: function() {},
+				listen: function(port, callback) {
+					callback();
+				}
+			});
 
-            netMock.expects('createServer')
-                .once()
-                .returns(listenerTest.object);
+			netMock.expects('createServer')
+				.once()
+				.returns(listenerTest.object);
 
-            listenerTest.expects('on')
-                .once()
-                .withArgs('error');
+			listenerTest.expects('on')
+				.once()
+				.withArgs('error');
 
-            listenerTest.expects('listen')
-                .once()
-                .withArgs(port);
+			listenerTest.expects('listen')
+				.once()
+				.withArgs(port);
 
-            testModule.listen(serverTest);
+			testModule.listen(serverTest);
 
-            expect(serverTest.listener).to.be.not.null;
-            listenerTest.verify();
-            netMock.verify();
-            done();
-        });
-    });
+			expect(serverTest.listener).to.be.not.null;
+			listenerTest.verify();
+			netMock.verify();
+			done();
+		});
+	});
 
-    describe('#stop(server, callback)', () => {
-        it('should disconnect all sockets and close the server', (done) => {
-            var clientStub = sinon.stub(testModule, 'disconnect');
-            var serverClose = sinon.spy();
+	describe('#stop(server, callback)', () => {
+		it('should disconnect all sockets and close the server', (done) => {
+			var clientStub = sinon.stub(testModule, 'disconnect');
+			var serverClose = sinon.spy();
 
-            var testServer = {
-                listener: {
-                    close: serverClose
-                }
-            };
+			var testServer = {
+				listener: {
+					close: serverClose
+				}
+			};
 
-            testModule.stop(testServer);
+			testModule.stop(testServer);
 
-            expect(serverClose.calledOnce).to.be.true;
-            clientStub.restore();
-            done();
-        });
-    });
+			expect(serverClose.calledOnce).to.be.true;
+			clientStub.restore();
+			done();
+		});
+	});
 
-    describe('#send(socket, payload)', () => {
-        it('should send the payload through the socket', () => {
-            var testPayload = new Buffer(JSON.stringify({foo: 'bar'}));
-            var socketMock = sinon.mock({
-                write: function () {
-                }
-            });
+	describe('#send(socket, payload)', () => {
+		it('should send the payload through the socket', () => {
+			var testPayload = new Buffer(JSON.stringify({foo: 'bar'}));
+			var socketMock = sinon.mock({
+				write: function() {}
+			});
 
-            socketMock.expects('write')
-                .twice();
+			socketMock.expects('write')
+				.twice();
 
-            testModule.send(socketMock.object, testPayload);
-            socketMock.verify();
-        });
-    });
+			testModule.send(socketMock.object, testPayload);
+			socketMock.verify();
+		});
+	});
 
-    describe('#createSocket(client, socket)', () => {
-        it('should create a socket client and return it', () => {
-            var netMock = sinon.mock(net);
+	describe('#createSocket(client, socket)', () => {
+		it('should create a socket client and return it', () => {
+			var netMock = sinon.mock(net);
 
-            netMock.expects('connect')
-                .once()
-                .withArgs(9000, '0.0.0.0')
-                .returns({
-                    on: function () {
-                    },
-                    pipe: function () {
-                        return new EventEmitter();
-                    },
-                    setTimeout: function () {
-                    },
-                    connect: function () {
-                    }
-                });
+			netMock.expects('connect')
+				.once()
+				.withArgs(9000, '0.0.0.0')
+				.returns({
+					on: function() {},
+					pipe: function() {
+						return new EventEmitter();
+					},
+					setTimeout: function() {},
+					connect: function() {}
+				});
 
-            var result = testModule.createSocket({
-                options: {
-                    hostname: '0.0.0.0',
-                    port: 9000
-                },
-                handleError: function () {
-                },
-                handleConnect: function () {
-                },
-                handleRequest: function () {
-                },
-                handleDisconnect: function () {
-                }
-            });
+			var result = testModule.createSocket({
+				options: {
+					hostname: '0.0.0.0',
+					port: 9000
+				},
+				handleError: function() {},
+				handleConnect: function() {},
+				handleRequest: function() {},
+				handleDisconnect: function() {}
+			});
 
-            netMock.verify();
-        });
-    });
+			netMock.verify();
+		});
+	});
 
-    describe('#disconnect(client)', () => {
-        it('should call the client\'s disconnect method', (done) => {
-            var testSocket = {
-                destroy: function () {
-                }
-            };
-            var clientMock = sinon.mock({
-                handleDisconnect: function () {
-                },
-                socket: testSocket
-            });
+	describe('#disconnect(client)', () => {
+		it('should call the client\'s disconnect method', (done) => {
+			var testSocket = {
+				destroy: function() {}
+			};
+			var clientMock = sinon.mock({
+				handleDisconnect: function() {},
+				socket: testSocket
+			});
 
-            clientMock.expects('handleDisconnect')
-                .once();
+			clientMock.expects('handleDisconnect')
+				.once();
 
-            testModule.disconnect(clientMock.object);
+			testModule.disconnect(clientMock.object);
 
-            setTimeout(() => {
-                clientMock.verify();
-                done();
-            }, 10);
-        });
-    });
+			setTimeout(() => {
+				clientMock.verify();
+				done();
+			}, 10);
+		});
+	});
 });
--- tests/unit/adapters/udp.js
+++ tests/unit/adapters/udp.js
@@ -12,162 +12,153 @@ var sinon = require('sinon');
 var testModule = require('../../../src/adapters/udp');
 
 var dgram = require('dgram');
+// import {afterEach, it, describe} from "mocha";
 
 /* Tests ---------------------------------------------------------------------*/
 
 describe('UDP', () => {
-    describe('#listen(server, callback)', () => {
+	describe('#listen(server, callback)', () => {
 
-        afterEach(() => sinon.mock.restore());
+		afterEach(() => sinon.mock.restore());
 
-        it('should bind to a defined port', (done) => {
+		it('should bind to a defined port', (done) => {
 
-            var dgramMock = sinon.mock(dgram);
-            var port = 9000;
-            var serverTest = {
-                listener: null,
-                handleError: function () {
-                },
-                options: {
-                    port: port
-                }
-            };
-            var listenerTest = sinon.mock({
-                on: function () {
-                },
-                bind: function () {
-                }
-            });
+			var dgramMock = sinon.mock(dgram);
+			var port = 9000;
+			var serverTest = {
+				listener: null,
+				handleError: function() {},
+				options: {
+					port: port
+				}
+			};
+			var listenerTest = sinon.mock({
+				on: function() {},
+				bind: function() {}
+			});
 
-            dgramMock.expects('createSocket')
-                .once()
-                .withArgs({
-                    type: 'udp4',
-                    reuseAddr: true
-                })
-                .returns(listenerTest.object);
+			dgramMock.expects('createSocket')
+				.once()
+				.withArgs({
+					type: 'udp4',
+					reuseAddr: true
+				})
+				.returns(listenerTest.object);
 
-            listenerTest.expects('on')
-                .twice();
+			listenerTest.expects('on')
+				.twice();
 
-            listenerTest.expects('bind')
-                .once()
-                .withArgs(port, '0.0.0.0');
+			listenerTest.expects('bind')
+				.once()
+				.withArgs(port, '0.0.0.0');
 
-            testModule.listen(serverTest, () => {
-                expect(serverTest.listener).to.be.not.null;
-                listenerTest.verify();
-                dgramMock.verify();
-                done();
-            });
+			testModule.listen(serverTest, () => {
+				expect(serverTest.listener).to.be.not.null;
+				listenerTest.verify();
+				dgramMock.verify();
+				done();
+			});
 
-        });
+		});
 
-        describe('#stop(server, callback)', () => {
-            it('should disconnect all sockets and close the server', (done) => {
-                var clientStub = sinon.stub(testModule, 'disconnect');
-                var serverClose = sinon.spy();
-                var clients = {
-                    a: 'a',
-                    b: 'b',
-                    c: 'c'
-                };
+		describe('#stop(server, callback)', () => {
+			it('should disconnect all sockets and close the server', (done) => {
+				var clientStub = sinon.stub(testModule, 'disconnect');
+				var serverClose = sinon.spy();
+				var clients = {
+					a: 'a',
+					b: 'b',
+					c: 'c'
+				};
 
-                var testServer = {
-                    __clients: clients,
-                    listener: {
-                        close: serverClose
-                    }
-                };
+				var testServer = {
+					__clients: clients,
+					listener: {
+						close: serverClose
+					}
+				};
 
-                testModule.stop(testServer, () => {
-                    expect(clientStub.withArgs('a').called).to.be.true;
-                    expect(clientStub.withArgs('b').called).to.be.true;
-                    expect(clientStub.withArgs('c').called).to.be.true;
-                    expect(serverClose.calledOnce).to.be.true;
-                    clientStub.restore();
-                    done();
-                });
-            });
-        });
+				testModule.stop(testServer, () => {
+					expect(clientStub.withArgs('a').called).to.be.true;
+					expect(clientStub.withArgs('b').called).to.be.true;
+					expect(clientStub.withArgs('c').called).to.be.true;
+					expect(serverClose.calledOnce).to.be.true;
+					clientStub.restore();
+					done();
+				});
+			});
+		});
 
-        describe('#send(socket, payload)', () => {
-            it('should send the payload through the socket', () => {
-                var testPayload = new Buffer(JSON.stringify({foo: 'bar'}));
-                var socketMock = sinon.mock({
-                    send: function () {
-                    },
-                    __port: 9000,
-                    __hostname: '0.0.0.0'
-                });
+		describe('#send(socket, payload)', () => {
+			it('should send the payload through the socket', () => {
+				var testPayload = new Buffer(JSON.stringify({foo: 'bar'}));
+				var socketMock = sinon.mock({
+					send: function() {},
+					__port: 9000,
+					__hostname: '0.0.0.0'
+				});
 
-                socketMock.expects('send')
-                    .once()
-                    .withArgs(
-                        testPayload,
-                        0,
-                        testPayload.length,
-                        9000,
-                        '0.0.0.0'
-                    );
+				socketMock.expects('send')
+					.once()
+					.withArgs(
+						testPayload,
+						0,
+						testPayload.length,
+						9000,
+						'0.0.0.0'
+					);
 
-                testModule.send(socketMock.object, testPayload);
-                socketMock.verify();
-            });
-        });
+				testModule.send(socketMock.object, testPayload);
+				socketMock.verify();
+			});
+		});
 
-        describe('#createSocket(client, socket)', () => {
-            it('should create a socket client and return it', () => {
-                var dgramMock = sinon.mock(dgram);
+		describe('#createSocket(client, socket)', () => {
+			it('should create a socket client and return it', () => {
+				var dgramMock = sinon.mock(dgram);
 
-                dgramMock.expects('createSocket')
-                    .once()
-                    .withArgs('udp4')
-                    .returns({
-                        on: function () {
-                        },
-                        bind: function () {
-                        }
-                    });
+				dgramMock.expects('createSocket')
+					.once()
+					.withArgs('udp4')
+					.returns({
+						on: function() {},
+						bind: function() {}
+					});
 
-                var result = testModule.createSocket({
-                    options: {
-                        hostname: '0.0.0.0',
-                        port: 9000
-                    },
-                    handleError: function () {
-                    },
-                    handleConnect: function () {
-                    },
-                    handleRequest: function () {
-                    }
-                });
+				var result = testModule.createSocket({
+					options: {
+						hostname: '0.0.0.0',
+						port: 9000
+					},
+					handleError: function() {},
+					handleConnect: function() {},
+					handleRequest: function() {}
+				});
 
-                expect(result.__port).to.equal(9000);
-                expect(result.__hostname).to.equal('0.0.0.0');
-                dgramMock.verify();
-            });
-        });
+				expect(result.__port).to.equal(9000);
+				expect(result.__hostname).to.equal('0.0.0.0');
+				dgramMock.verify();
+			});
+		});
 
-        describe('#disconnect(client)', () => {
-            it('should call the client\'s disconnect method', (done) => {
-                var testSocket = {foo: 'bar'};
-                var clientMock = sinon.mock({
-                    handleDisconnect: function () {
-                    },
-                    socket: testSocket
-                });
+		describe('#disconnect(client)', () => {
+			it('should call the client\'s disconnect method', (done) => {
+				var testSocket = {foo: 'bar'};
+				var clientMock = sinon.mock({
+					handleDisconnect: function() {},
+					socket: testSocket
+				});
 
-                clientMock.expects('handleDisconnect')
-                    .once();
+				clientMock.expects('handleDisconnect')
+					.once();
 
-                testModule.disconnect(clientMock.object);
+				testModule.disconnect(clientMock.object);
 
-                setTimeout(() => {
-                    clientMock.verify();
-                    done();
-                }, 10);
-            });
-        });
-    });
+				setTimeout(() => {
+					clientMock.verify();
+					done();
+				}, 10);
+			});
+		});
+	});
 });
